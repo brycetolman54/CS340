0f8880956080d7842b1df7b7884468e351b2871f
brycealantolman@gmail.com <brycealantolman@gmail.com>
2025-03-24 02:07:03 -0600

    getMoreFolloweesLambda finished pt 2

---
+++ tweeter/tweeter-server/src/model/service/FollowService.ts
@@ -0,0 +1,35 @@
+import { UserDto, FakeData, User } from "tweeter-shared";
+
+export class FollowService {
+    public async loadMoreFollowers(
+        token: string,
+        userAlias: string,
+        pageSize: number,
+        lastItem: UserDto | null
+    ): Promise<[UserDto[], boolean]> {
+        return this.getFakeData(lastItem, pageSize, userAlias);
+    }
+
+    public async loadMoreFollowees(
+        token: string,
+        userAlias: string,
+        pageSize: number,
+        lastItem: UserDto | null
+    ): Promise<[UserDto[], boolean]> {
+        return this.getFakeData(lastItem, pageSize, userAlias);
+    }
+
+    private async getFakeData(
+        lastItem: UserDto | null,
+        pageSize: number,
+        userAlias: string
+    ): Promise<[UserDto[], boolean]> {
+        const [items, hasMore] = FakeData.instance.getPageOfUsers(
+            User.fromDto(lastItem),
+            pageSize,
+            userAlias
+        );
+        const dtos = items.map((user) => user.dto);
+        return [dtos, hasMore];
+    }
+}
